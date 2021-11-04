dfdab0027a3d787744bf19b22aadd30ae469f3ac
martinohmann <martinohmann@gmail.com>
2021-11-04 22:32:33 +0100

    feat: add jsonpath and flatten transformations

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@
 #![deny(missing_docs)]
 
 use anyhow::{anyhow, Context, Result};
-use clap::{Parser, ValueHint};
+use clap::{Args, Parser, ValueHint};
+use jsonpath_rust::JsonPathQuery;
 use std::collections::VecDeque;
 use std::path::{Path, PathBuf};
 
@@ -13,24 +14,36 @@ use trnscd::{
 };
 
 /// Simple tool to transcode between different encodings.
-#[derive(Parser, Debug, Clone)]
+#[derive(Parser, Debug)]
 #[clap(name = "trnscd", version)]
 struct Options {
+    /// If stdin is not a pipe, the first file is read from. Otherwise it is treated as the output
+    /// file. It is possible to provide multiple output files if the data resembles an array. Each
+    /// output file will receive an array element. The last output file collects the remaining
+    /// elements if there are more elements than files. Passing '-' as filename or providing no
+    /// output files will write the data to stdout instead
+    #[clap(name = "FILE", parse(from_os_str), value_hint = ValueHint::FilePath)]
+    files: Vec<PathBuf>,
+
+    /// Options for deserializing the input.
+    #[clap(flatten)]
+    input: InputOptions,
+
+    /// Options for serializing the output.
+    #[clap(flatten)]
+    output: OutputOptions,
+
+    /// Options for data transformations performed after deserializing from the input encoding but
+    /// before serializing to the output encoding.
+    #[clap(flatten)]
+    transform: TransformOptions,
+}
+
+#[derive(Args, Debug)]
+struct InputOptions {
     /// Input encoding, if absent encoding will be detected from input file extension
     #[clap(arg_enum, short = 'i', long)]
     input_encoding: Option<Encoding>,
-
-    /// Output encoding, if absent encoding will be detected from output file extension
-    #[clap(arg_enum, short = 'o', long)]
-    output_encoding: Option<Encoding>,
-
-    /// Produce pretty output if supported by the encoder
-    #[clap(short = 'p', long)]
-    pretty: bool,
-
-    /// Add a trailing newline to the output
-    #[clap(short = 'n', long)]
-    newline: bool,
 
     /// Deserialize inputs that can contain multiple documents (e.g. YAML) into an array.
     /// Otherwise, only deserialize the first document
@@ -47,6 +60,21 @@ struct Options {
     /// into an array of arrays
     #[clap(long)]
     csv_headers_as_keys: bool,
+}
+
+#[derive(Args, Debug)]
+struct OutputOptions {
+    /// Output encoding, if absent encoding will be detected from output file extension
+    #[clap(arg_enum, short = 'o', long)]
+    output_encoding: Option<Encoding>,
+
+    /// Produce pretty output if supported by the encoder
+    #[clap(short = 'p', long)]
+    pretty: bool,
+
+    /// Add a trailing newline to the output
+    #[clap(short = 'n', long)]
+    newline: bool,
 
     /// When the input is an array of objects and the output encoding is CSV or TSV, the field
     /// names of the first object will be used as CSV headers. Field values of all following
@@ -54,17 +82,28 @@ struct Options {
     /// empty columns while excess fields are ignored.
     #[clap(long)]
     keys_as_csv_headers: bool,
-
-    /// If stdin is not a pipe, the first file is read from. Otherwise it is treated as the output
-    /// file. It is possible to provide multiple output files if the data resembles an array. Each
-    /// output file will receive an array element. The last output file collects the remaining
-    /// elements if there are more elements than files. Passing '-' as filename or providing no
-    /// output files will write the data to stdout instead
-    #[clap(name = "FILE", parse(from_os_str), value_hint = ValueHint::FilePath)]
-    files: Vec<PathBuf>,
 }
 
-fn deserialize<P>(file: Option<P>, opts: &Options) -> Result<Value>
+#[derive(Args, Debug)]
+struct TransformOptions {
+    /// Select data from the decoded input via jsonpath before serializing it into the output
+    /// encoding. See https://docs.rs/jsonpath-rust/0.1.3/jsonpath_rust/index.html#operators for
+    /// supported operators.
+    ///
+    /// When using a jsonpath, the result will always be shaped like an array with zero or more
+    /// elements. See --flatten if you want to remove one level of nesting on single element
+    /// jsonpath filter results.
+    #[clap(short = 'j', long)]
+    jsonpath: Option<String>,
+
+    /// If the data is shaped like an array and has only one element, flatten it to the element by
+    /// removing one level of nesting. This is applied as the last transformation before
+    /// serializing into the output encoding.
+    #[clap(long)]
+    flatten: bool,
+}
+
+fn deserialize<P>(file: Option<P>, opts: &InputOptions) -> Result<Value>
 where
     P: AsRef<Path>,
 {
@@ -83,7 +122,23 @@ where
         .context(format!("failed to deserialize {}", encoding))
 }
 
-fn serialize<P>(file: Option<P>, value: &Value, opts: &Options) -> Result<()>
+fn transform(mut value: Value, opts: &TransformOptions) -> Result<Value> {
+    if let Some(selector) = &opts.jsonpath {
+        value = value
+            .path(selector)
+            .map_err(|e| anyhow!("invalid jsonpath:\n{}", e))?;
+    }
+
+    if let Some(array) = value.as_array() {
+        if opts.flatten && array.len() == 1 {
+            return Ok(array[0].clone());
+        }
+    }
+
+    Ok(value)
+}
+
+fn serialize<P>(file: Option<P>, value: &Value, opts: &OutputOptions) -> Result<()>
 where
     P: AsRef<Path>,
 {
@@ -118,10 +173,12 @@ fn main() -> Result<()> {
         false => None,
     };
 
-    let mut value = deserialize(input_file, &opts)?;
+    let value = deserialize(input_file, &opts.input)?;
+
+    let mut value = transform(value, &opts.transform)?;
 
     if files.len() <= 1 {
-        serialize(files.get(0), &value, &opts)
+        serialize(files.get(0), &value, &opts.output)
     } else {
         let values = match value.as_array_mut() {
             Some(values) => {
@@ -144,6 +201,6 @@ fn main() -> Result<()> {
         files
             .iter()
             .zip(values.iter())
-            .try_for_each(|(file, value)| serialize(Some(file), value, &opts))
+            .try_for_each(|(file, value)| serialize(Some(file), value, &opts.output))
     }
 }
