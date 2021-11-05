2a99411156f39c96dae7f195f952bb0506d31e95
martinohmann <martinohmann@gmail.com>
2021-11-05 18:38:49 +0100

    chore: provide short and long docs for all flags and args

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 #![deny(missing_docs)]
 
 use anyhow::{anyhow, Context, Result};
-use clap::{Args, Parser, ValueHint};
+use clap::{ArgSettings, Args, Parser, ValueHint};
 use jsonpath_rust::JsonPathQuery;
 use std::collections::VecDeque;
 use std::path::{Path, PathBuf};
@@ -16,14 +16,25 @@ use trnscd::{
 };
 
 /// Simple tool to transcode between different encodings.
+///
+/// The tool first deserializes data from the input data into an internal representation which
+/// resembles JSON. As an optional step certain transformations can be applied to the data before
+/// serializing into the output encoding.
+///
+/// Refer to the input, transform and output options below.
 #[derive(Parser, Debug)]
 #[clap(name = "trnscd", version)]
 struct Options {
-    /// If stdin is not a pipe, the first file is read from. Otherwise it is treated as the output
-    /// file. It is possible to provide multiple output files if the data resembles an array. Each
-    /// output file will receive an array element. The last output file collects the remaining
-    /// elements if there are more elements than files. Passing '-' as filename or providing no
-    /// output files will write the data to stdout instead
+    /// Input and output files.
+    ///
+    /// If stdin is not a pipe, the first file is the input file that is read from. Otherwise it is
+    /// treated as an output file.
+    ///
+    /// It is possible to provide multiple output files if the data resembles an array. Each output
+    /// file will receive an array element. The last output file collects the remaining elements if
+    /// there are more elements than files.
+    ///
+    /// Passing '-' as filename or providing no output files will write the data to stdout instead.
     #[clap(name = "FILE", parse(from_os_str), value_hint = ValueHint::FilePath)]
     files: Vec<PathBuf>,
 
@@ -31,36 +42,41 @@ struct Options {
     #[clap(flatten)]
     input: InputOptions,
 
-    /// Options for serializing the output.
-    #[clap(flatten)]
-    output: OutputOptions,
-
     /// Options for data transformations performed after deserializing from the input encoding but
     /// before serializing to the output encoding.
     #[clap(flatten)]
     transform: TransformOptions,
+
+    /// Options for serializing the output.
+    #[clap(flatten)]
+    output: OutputOptions,
 }
 
 #[derive(Args, Debug)]
 #[clap(help_heading = "INPUT OPTIONS")]
 struct InputOptions {
-    /// Input encoding, if absent encoding will be detected from input file extension
-    #[clap(arg_enum, short = 'i', long)]
+    /// Set the input encoding. If absent encoding will be detected from input file extension.
+    #[clap(arg_enum, short = 'i', long, setting = ArgSettings::HidePossibleValues)]
     input_encoding: Option<Encoding>,
 
     /// Deserialize inputs that can contain multiple documents (e.g. YAML) into an array.
-    /// Otherwise, only deserialize the first document
+    ///
+    /// Otherwise, only deserialize the first document.
     #[clap(short = 'A', long)]
     all_documents: bool,
 
-    /// If this flag is absent, the first line of CSV or TSV input is treated as headers and will
-    /// be discarded
+    /// Indicate that CSV input does not include a header row.
+    ///
+    /// If this flag is absent, the first line of CSV input is treated as headers and will be
+    /// discarded.
     #[clap(long)]
     csv_without_headers: bool,
 
-    /// When reading CSV or TSV, this flag will deserialize the input into an array of maps with
-    /// each field keyed by the corresponding header value. Otherwise, the input is deserialized
-    /// into an array of arrays
+    /// Use CSV headers as keys for the row columns.
+    ///
+    /// When reading CSV, this flag will deserialize the input into an array of maps with each
+    /// field keyed by the corresponding header value. Otherwise, the input is deserialized into an
+    /// array of arrays.
     #[clap(long)]
     csv_headers_as_keys: bool,
 }
@@ -76,24 +92,53 @@ impl From<&InputOptions> for DeserializeOptions {
 }
 
 #[derive(Args, Debug)]
+#[clap(help_heading = "TRANSFORM OPTIONS")]
+struct TransformOptions {
+    /// Select data from the decoded input via jsonpath query. Can be specified multiple times to
+    /// allow starting the filtering from the root element again.
+    ///
+    /// See https://docs.rs/jsonpath-rust/0.1.3/jsonpath_rust/index.html#operators for supported
+    /// operators.
+    ///
+    /// When using a jsonpath query, the result will always be shaped like an array with zero or
+    /// more elements. See --flatten if you want to remove one level of nesting on single element
+    /// filter results.
+    #[clap(short = 'j', long, multiple_occurrences = true, number_of_values = 1)]
+    jsonpath: Vec<String>,
+
+    /// Remove one level of nesting if the data is shaped like an array. Can be specified multiple
+    /// times.
+    ///
+    /// If the has only one element the array will be removed
+    /// entirely, leaving the single element as output.
+    ///
+    /// This is applied as the last transformation before serializing into the output encoding. Can
+    /// be used in combination with --jsonpath to flatten single element filter results.
+    #[clap(short, long, parse(from_occurrences))]
+    flatten: u8,
+}
+
+#[derive(Args, Debug)]
 #[clap(help_heading = "OUTPUT OPTIONS")]
 struct OutputOptions {
-    /// Output encoding, if absent encoding will be detected from output file extension
-    #[clap(arg_enum, short = 'o', long)]
+    /// Set the output encoding. If absent encoding will be detected from output file extension.
+    #[clap(arg_enum, short = 'o', long, setting = ArgSettings::HidePossibleValues)]
     output_encoding: Option<Encoding>,
 
-    /// Produce pretty output if supported by the encoder
+    /// Produce pretty output if supported by the encoder.
     #[clap(short = 'p', long)]
     pretty: bool,
 
-    /// Add a trailing newline to the output
+    /// Add a trailing newline to the output.
     #[clap(short = 'n', long)]
     newline: bool,
 
-    /// When the input is an array of objects and the output encoding is CSV or TSV, the field
-    /// names of the first object will be used as CSV headers. Field values of all following
-    /// objects will be matched to the right CSV column based on their key. Missing fields produce
-    /// empty columns while excess fields are ignored.
+    /// Use object keys of the first item as CSV headers.
+    ///
+    /// When the input is an array of objects and the output encoding is CSV, the field names of
+    /// the first object will be used as CSV headers. Field values of all following objects will be
+    /// matched to the right CSV column based on their key. Missing fields produce empty columns
+    /// while excess fields are ignored.
     #[clap(long)]
     keys_as_csv_headers: bool,
 }
@@ -106,32 +151,6 @@ impl From<&OutputOptions> for SerializeOptions {
             keys_as_csv_headers: opts.keys_as_csv_headers,
         }
     }
-}
-
-#[derive(Args, Debug)]
-#[clap(help_heading = "TRANSFORM OPTIONS")]
-struct TransformOptions {
-    /// Select data from the decoded input via jsonpath query.
-    ///
-    /// Can be specified multiple times to allow filtering intermediate results from the
-    /// root again. See https://docs.rs/jsonpath-rust/0.1.3/jsonpath_rust/index.html#operators for supported
-    /// operators.
-    ///
-    /// When using a jsonpath query, the result will always be shaped like an array with zero or
-    /// more elements. See --flatten if you want to remove one level of nesting on single element
-    /// filter results.
-    #[clap(short = 'j', long, multiple_occurrences = true, number_of_values = 1)]
-    jsonpath: Vec<String>,
-
-    /// Remove one level of nesting if the data is shaped like an array.
-    ///
-    /// Can be specified multiple times. If the has only one element the array will be removed
-    /// entirely, leaving the single element as output.
-    ///
-    /// This is applied as the last transformation before serializing into the output encoding. Can
-    /// be used in combination with --jsonpath to flatten single element filter results.
-    #[clap(short, long, parse(from_occurrences))]
-    flatten: u8,
 }
 
 fn deserialize<P>(file: Option<P>, opts: &InputOptions) -> Result<Value>
@@ -149,19 +168,20 @@ where
         .context(format!("failed to deserialize {}", encoding))
 }
 
-fn transform(mut value: Value, opts: &TransformOptions) -> Result<Value> {
+fn transform(value: &mut Value, opts: &TransformOptions) -> Result<()> {
     for selector in &opts.jsonpath {
-        value = value
+        *value = value
+            .clone()
             .path(selector)
             .map_err(|e| anyhow!(e))
             .context("invalid jsonpath query")?;
     }
 
     for _ in 0..opts.flatten {
-        flatten(&mut value)
+        flatten(value)
     }
 
-    Ok(value)
+    Ok(())
 }
 
 fn flatten(value: &mut Value) {
@@ -214,9 +234,9 @@ fn main() -> Result<()> {
         false => None,
     };
 
-    let value = deserialize(input_file, &opts.input)?;
+    let mut value = deserialize(input_file, &opts.input)?;
 
-    let mut value = transform(value, &opts.transform)?;
+    transform(&mut value, &opts.transform)?;
 
     if files.len() <= 1 {
         serialize(files.get(0), &value, &opts.output)
