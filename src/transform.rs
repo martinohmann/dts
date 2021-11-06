b612aeabc81ee2f418e2cc98a1a46108e5451ac1
martinohmann <martinohmann@gmail.com>
2021-11-06 13:46:57 +0100

    chore: add transform tests

--- src/transform.rs
+++ src/transform.rs
@@ -5,7 +5,33 @@ use jsonpath_rust::JsonPathQuery;
 
 /// Filter value in place according to the jsonpath query.
 ///
+/// ```
+/// # use dts::transform::filter_in_place;
+/// # use pretty_assertions::assert_eq;
+/// use serde_json::json;
+///
+/// let mut value = json!({
+///   "orders": [
+///     {"id": 1, "active": true},
+///     {"id": 2},
+///     {"id": 3},
+///     {"id": 4, "active": true}
+///   ]
+/// });
+///
+/// filter_in_place(&mut value, "$.orders[?(@.active)].id");
+/// assert_eq!(value, json!([1, 4]));
+/// ```
+///
 /// This can fail if parsing the query fails.
+///
+/// ```
+/// # use dts::transform::filter_in_place;
+/// use serde_json::json;
+///
+/// let mut value = json!([]);
+/// assert!(filter_in_place(&mut value, "$[").is_err());
+/// ```
 pub fn filter_in_place<Q>(value: &mut Value, query: Q) -> Result<()>
 where
     Q: AsRef<str>,
@@ -19,8 +45,43 @@ where
 
 /// Remove one level of nesting if the data is shaped like an array.
 ///
+/// ```
+/// # use dts::transform::flatten_in_place;
+/// # use pretty_assertions::assert_eq;
+/// use serde_json::json;
+///
+/// let mut value = json!([["foo"], ["bar"], [["baz"], "qux"]]);
+///
+/// flatten_in_place(&mut value);
+/// assert_eq!(value, json!(["foo", "bar", ["baz"], "qux"]));
+/// ```
+///
 /// If the has only one element the array will be removed entirely, leaving the single element as
 /// output.
+///
+/// ```
+/// # use dts::transform::flatten_in_place;
+/// # use pretty_assertions::assert_eq;
+/// use serde_json::json;
+///
+/// let mut value = json!(["foo"]);
+///
+/// flatten_in_place(&mut value);
+/// assert_eq!(value, json!("foo"));
+/// ```
+///
+/// Non-array values will be left untouched.
+///
+/// ```
+/// # use dts::transform::flatten_in_place;
+/// # use pretty_assertions::assert_eq;
+/// use serde_json::json;
+///
+/// let mut value = json!({"foo": "bar"});
+///
+/// flatten_in_place(&mut value);
+/// assert_eq!(value, json!({"foo": "bar"}));
+/// ```
 pub fn flatten_in_place(value: &mut Value) {
     if let Some(array) = value.as_array() {
         *value = if array.len() == 1 {
