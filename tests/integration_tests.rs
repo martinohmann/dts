832ed57284d4fe89ebab61dec866b0a0b6f993b3
martinohmann <martinohmann@gmail.com>
2021-11-08 18:55:10 +0100

    chore: add some integration tests

---
+++ tests/integration_tests.rs
@@ -0,0 +1,63 @@
+use assert_cmd::Command;
+use std::fs::read_to_string as read;
+
+#[test]
+fn json_to_yaml() {
+    Command::cargo_bin("dts")
+        .unwrap()
+        .args(&["-i", "json", "-o", "yaml"])
+        .pipe_stdin("tests/fixtures/example.json")
+        .unwrap()
+        .assert()
+        .success()
+        .code(0)
+        .stdout(read("tests/fixtures/example.yaml").unwrap());
+}
+
+#[test]
+fn yaml_to_pretty_json() {
+    Command::cargo_bin("dts")
+        .unwrap()
+        .args(&["-i", "yaml", "-o", "json", "-p", "-n"])
+        .pipe_stdin("tests/fixtures/example.yaml")
+        .unwrap()
+        .assert()
+        .success()
+        .code(0)
+        .stdout(read("tests/fixtures/example.json").unwrap());
+}
+
+#[test]
+fn json_to_toml() {
+    Command::cargo_bin("dts")
+        .unwrap()
+        .args(&["-i", "json", "-o", "toml"])
+        .pipe_stdin("tests/fixtures/example.json")
+        .unwrap()
+        .assert()
+        .success()
+        .code(0)
+        .stdout(read("tests/fixtures/example.toml").unwrap());
+}
+
+#[test]
+fn json_to_csv_filtered_flattened_with_keys() {
+    Command::cargo_bin("dts")
+        .unwrap()
+        .args(&[
+            "-i",
+            "json",
+            "-o",
+            "csv",
+            "-j",
+            "..friends",
+            "--flatten",
+            "--keys-as-csv-headers",
+        ])
+        .pipe_stdin("tests/fixtures/example.json")
+        .unwrap()
+        .assert()
+        .success()
+        .code(0)
+        .stdout(read("tests/fixtures/friends.csv").unwrap());
+}
