e8cccf3a5a03a5c3fbfa0bc270d68ebb37fba752
martinohmann <martinohmann@gmail.com>
2024-06-01 12:23:14 +0200

    chore: update jaq deps to 1.x

--- src/filter/jaq.rs
+++ src/filter/jaq.rs
@@ -1,14 +1,14 @@
 //! A wrapper for `jaq`.
 
 use crate::{Error, Result};
-use jaq_core::{Ctx, Definitions, RcIter, Val};
+use jaq_interpret::{Ctx, FilterT, ParseCtx, RcIter, Val};
 use serde_json::Value;
 use std::fmt;
 
 #[derive(Debug)]
 struct ParseError {
     expr: String,
-    errs: Vec<jaq_core::parse::Error>,
+    errs: Vec<jaq_parse::Error>,
 }
 
 impl fmt::Display for ParseError {
@@ -30,25 +30,21 @@ impl fmt::Display for ParseError {
 impl std::error::Error for ParseError {}
 
 pub(crate) struct Filter {
-    filter: jaq_core::Filter,
+    filter: jaq_interpret::Filter,
 }
 
 impl Filter {
     pub(crate) fn new(expr: &str) -> Result<Filter> {
-        let mut errs = Vec::new();
-        let mut defs = Definitions::core();
+        let mut defs = ParseCtx::new(Vec::new());
+        defs.insert_natives(jaq_core::core());
+        defs.insert_defs(jaq_std::std());
 
-        jaq_std::std()
-            .into_iter()
-            .for_each(|def| defs.insert(def, &mut errs));
-
-        assert!(errs.is_empty());
-
-        let (main, mut errs) = jaq_core::parse::parse(expr, jaq_core::parse::main());
-        let f = main.map(|main| defs.finish(main, Vec::new(), &mut errs));
+        let (main, errs) = jaq_parse::parse(expr, jaq_parse::main());
 
         if errs.is_empty() {
-            Ok(Filter { filter: f.unwrap() })
+            Ok(Filter {
+                filter: defs.compile(main.unwrap()),
+            })
         } else {
             Err(Error::new(ParseError {
                 expr: expr.to_owned(),
@@ -62,7 +58,7 @@ impl Filter {
         let iter = RcIter::new(empty.into_iter());
         let mut values = self
             .filter
-            .run(Ctx::new(Vec::new(), &iter), Val::from(value))
+            .run((Ctx::new(Vec::new(), &iter), Val::from(value)))
             .map(|out| Ok(Value::from(out.map_err(Error::new)?)))
             .collect::<Result<Vec<_>, Error>>()?;
 
