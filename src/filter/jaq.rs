ac4ec17223c7960bce7f3448a292a0ef172bba4f
martinohmann <martinohmann@gmail.com>
2025-01-22 21:32:45 +0100

    chore: migrate to jaq 2.x

--- src/filter/jaq.rs
+++ src/filter/jaq.rs
@@ -1,14 +1,16 @@
 //! A wrapper for `jaq`.
 
 use crate::{Error, Result};
-use jaq_interpret::{Ctx, FilterT, ParseCtx, RcIter, Val};
+use jaq_core::load::{Arena, File, Loader};
+use jaq_core::{compile, load, Compiler, Ctx, Native, RcIter};
+use jaq_json::Val;
 use serde_json::Value;
 use std::fmt;
 
 #[derive(Debug)]
 struct ParseError {
     expr: String,
-    errs: Vec<jaq_parse::Error>,
+    errs: Vec<String>,
 }
 
 impl fmt::Display for ParseError {
@@ -30,27 +32,37 @@ impl fmt::Display for ParseError {
 impl std::error::Error for ParseError {}
 
 pub(crate) struct Filter {
-    filter: jaq_interpret::Filter,
+    filter: jaq_core::Filter<Native<Val>>,
 }
 
 impl Filter {
     pub(crate) fn new(expr: &str) -> Result<Filter> {
-        let mut defs = ParseCtx::new(Vec::new());
-        defs.insert_natives(jaq_core::core());
-        defs.insert_defs(jaq_std::std());
+        let program = File {
+            code: expr,
+            path: (),
+        };
 
-        let (main, errs) = jaq_parse::parse(expr, jaq_parse::main());
+        let loader = Loader::new(jaq_std::defs().chain(jaq_json::defs()));
+        let arena = Arena::default();
 
-        if errs.is_empty() {
-            Ok(Filter {
-                filter: defs.compile(main.unwrap()),
-            })
-        } else {
-            Err(Error::new(ParseError {
+        let modules = loader.load(&arena, program).map_err(|errs| {
+            Error::new(ParseError {
                 expr: expr.to_owned(),
-                errs,
-            }))
-        }
+                errs: load_errors(errs),
+            })
+        })?;
+
+        let filter = Compiler::default()
+            .with_funs(jaq_std::funs().chain(jaq_json::funs()))
+            .compile(modules)
+            .map_err(|errs| {
+                Error::new(ParseError {
+                    expr: expr.to_owned(),
+                    errs: compile_errors(errs),
+                })
+            })?;
+
+        Ok(Filter { filter })
     }
 
     pub(crate) fn apply(&self, value: Value) -> Result<Value> {
@@ -67,5 +79,42 @@ impl Filter {
         } else {
             Ok(Value::Array(values))
         }
+    }
+}
+
+fn load_errors(errs: load::Errors<&str, ()>) -> Vec<String> {
+    errs.into_iter()
+        .map(|(_, err)| match err {
+            load::Error::Io(errs) => errs.into_iter().map(report_io).collect(),
+            load::Error::Lex(errs) => errs.into_iter().map(report_lex).collect(),
+            load::Error::Parse(errs) => errs.into_iter().map(report_parse).collect(),
+        })
+        .collect()
+}
+
+fn compile_errors(errs: compile::Errors<&str, ()>) -> Vec<String> {
+    errs.into_iter()
+        .flat_map(|(_, errs)| errs.into_iter().map(report_compile))
+        .collect()
+}
+
+fn report_io((path, error): (&str, String)) -> String {
+    format!("could not load file {}: {}", path, error)
+}
+
+fn report_lex((expected, _found): load::lex::Error<&str>) -> String {
+    format!("expected {}", expected.as_str())
+}
+
+fn report_parse((expected, _found): load::parse::Error<&str>) -> String {
+    format!("expected {}", expected.as_str())
+}
+
+fn report_compile((found, undefined): compile::Error<&str>) -> String {
+    let wnoa = |exp, got| format!("wrong number of arguments (expected {exp}, found {got})");
+    match (found, undefined) {
+        ("reduce", compile::Undefined::Filter(arity)) => wnoa("2", arity),
+        ("foreach", compile::Undefined::Filter(arity)) => wnoa("2 or 3", arity),
+        (_, undefined) => format!("undefined {}", undefined.as_str()),
     }
 }
